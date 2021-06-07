92e132e11e5652fa673175c6c7f9fe835e820265
Alejandro Sanchez <alex.9802@hotmail.com>
2021-06-07 14:42:43 -0500

    Stage 7 section 63

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,43 +1,104 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final _textController = new TextEditingController();
+  final _focusNode = new FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: Column(
-            children: [
-              CircleAvatar(
-                child: Text('Te', style: TextStyle(fontSize: 12)),
-                backgroundColor: Colors.blue[100],
-                maxRadius: 14,
-              ),
-              SizedBox(height: 3),
-              Text(
-                'melisa Flores',
-                style: TextStyle(color: Colors.black87, fontSize: 12),
-              )
-            ],
-          ),
-          centerTitle: true,
-          elevation: 1,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              child: Text('Te', style: TextStyle(fontSize: 12)),
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+            ),
+            SizedBox(height: 3),
+            Text(
+              'Melisa Flores',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
         ),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              Flexible(
-                child: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  itemBuilder: (_, i) => Text('$i'),
-                  reverse: true,
-                ),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, i) => Text('$i'),
+                reverse: true,
               ),
-              Divider(height: 1),
-              // TODO: Caja de texto
-              Container(color: Colors.white, height: 100)
-            ],
+            ),
+            Divider(height: 1),
+            // TODO: Caja de texto
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: _inputChat(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _inputChat() {
+    return SafeArea(
+        child: Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmit,
+              onChanged: (String text) {
+                // TODO: Cuando hay un valor para poder postear
+              },
+              decoration: InputDecoration.collapsed(
+                hintText: 'Send Message',
+              ),
+              focusNode: _focusNode,
+            ),
           ),
-        ));
+          // Send Button
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4.0),
+            child: Platform.isIOS
+                ? CupertinoButton(
+                    child: Text('Send'),
+                    onPressed: () {},
+                  )
+                : Container(
+                    margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                      icon: Icon(Icons.send, color: Colors.blue[400]),
+                      onPressed: () {},
+                    ),
+                  ),
+          ),
+        ],
+      ),
+    ));
+  }
+
+  _handleSubmit(String texto) {
+    print(texto);
+    _textController.clear();
+    _focusNode.requestFocus();
   }
 }
