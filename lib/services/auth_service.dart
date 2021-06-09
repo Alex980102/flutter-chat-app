0066052d553488558fdbbc44dac87b565b6aea4e
Alejandro Sanchez <alex.9802@hotmail.com>
2021-06-09 10:34:29 -0500

    Stage 7 section 91 Universal

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:chat_app_flutter/global/environment.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService with ChangeNotifier {
+  Future login(String email, String password) async {
+    final data = {'email': email, 'password': password};
+    final res = await http.post(Uri.parse('${Environment.apiUrl}/login'),
+        body: jsonEncode(data), headers: {'Content-Type': 'application/json'});
+    print(res.body);
+  }
+}
