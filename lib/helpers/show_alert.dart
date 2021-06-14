713e9e2a50d44b46ec1b630ed6f7154a5d89200e
Alejandro Sánchez <alex.980102@gmail.com>
2021-06-14 13:25:47 -0500

    Stage 7 section 94.2 Apple

--- lib/helpers/show_alert.dart
+++ lib/helpers/show_alert.dart
@@ -1,7 +1,27 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 showAlert(BuildContext context, String title, String subtitle) {
-  showDialog(
+  if (Platform.isIOS || Platform.isMacOS) {
+    return showCupertinoDialog(
+      context: context,
+      builder: (_) => CupertinoAlertDialog(
+        title: Text(title),
+        content: Text(subtitle),
+        actions: <Widget>[
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: Text('Ok'),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  return showDialog(
       context: context,
       builder: (_) => AlertDialog(
             title: Text(title),
